d8a6b5b1505540ae89bfa3a1df0195551e9dc998
Srivathsan <srivathsan@ubuntu.(none)>
2013-01-10 21:49:16 +0530

    Works but seems buggy. Doesn't detect self win.

--- TicTacToe/T2.cpp
+++ TicTacToe/T2.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+int calcRowSum(int T3[][3], int rowNumber)
+{	return(T3[rowNumber][0]+T3[rowNumber][1]+T3[rowNumber][2]);}
+
+int calcColSum(int T3[][3], int colNumber)
+{	return(T3[0][colNumber]+T3[1][colNumber]+T3[2][colNumber]);}
+
+int calcDiagSum(int T3[][3], int diagNumber)
+{
+	int diagSum;
+	switch(diagNumber)
+	{
+		case 1:
+				diagSum = T3[0][0]+T3[1][1]+T3[2][2];
+				break;
+		case 2:
+				diagSum = T3[0][2]+T3[1][1]+T3[2][0];
+				break;
+		default:
+			cout << "error in diagSum Call";
+	}
+	return(diagSum);
+}
+
+
 void dispT3(int T3[][3])
 {
 	cout << endl << endl;
@@ -21,13 +45,13 @@ void dispT3(int T3[][3])
 					dispT[j] = 'X';
 					break;
 
-				case 2:
+				case -1:
 					dispT[j] = 'O';
 					break;
 
 				default:
 					//error
-					cout << "Error";
+					cout << "Error in display";
 			}		
 		}
 
@@ -35,6 +59,103 @@ void dispT3(int T3[][3])
 	}
 }
 
+
+
+
+struct checkSumReturn{
+	int victoryFlag, rowNumber,colNumber, diagNumber;
+};
+
+checkSumReturn checkForSumValue(int T3[][3], int sumValue)
+{
+		
+	checkSumReturn returnStruct;
+	returnStruct.victoryFlag=0; 
+	//int victoryFlag=0;
+	// victoryFlag=0 => no victory
+	// victoryFlag=1 => user victory 
+	// victoryFlag=-1 => my victory
+
+	// 8 possible victory scenarios
+	// row-wise
+	for(int i=0;i<3;i++)
+	{	int rowSum=calcRowSum(T3,i);
+		if((rowSum==sumValue)||(rowSum==-sumValue))
+		{
+			if(rowSum==sumValue)
+				returnStruct.victoryFlag = 1;
+			else //rowSum==-sumValue
+				returnStruct.victoryFlag = -1;
+			// return row Number
+			returnStruct.rowNumber = i;
+			returnStruct.colNumber = -1;
+			returnStruct.diagNumber = -1;
+			return(returnStruct);
+		}
+	}
+
+	// column-wise
+	for(int i=0;i<3;i++)
+	{ int colSum = calcColSum(T3,i);
+		if((colSum==sumValue)||(colSum==-sumValue))
+		{
+			if(colSum==sumValue)
+				returnStruct.victoryFlag = 1;
+			else //colSum==-sumValue
+				returnStruct.victoryFlag = -1;
+			// return col Number
+			returnStruct.rowNumber = -1;
+			returnStruct.colNumber = i;
+			returnStruct.diagNumber = -1;
+			return(returnStruct);
+		}
+	}
+
+	// diagonals
+	{
+		 int diag1Sum = calcDiagSum(T3,1);
+		 if((diag1Sum==sumValue)||(diag1Sum==-sumValue))
+		{
+			if(diag1Sum==sumValue)
+				returnStruct.victoryFlag = 1;
+			else //diag1Sum==-sumValue
+				returnStruct.victoryFlag = -1;
+			// return -1 for both 
+			returnStruct.rowNumber = -1;
+			returnStruct.colNumber = -1;
+			returnStruct.diagNumber = 1;
+			return(returnStruct);
+		}
+		 
+		 int diag2Sum = calcDiagSum(T3,2);
+		 if((diag2Sum==sumValue)||(diag2Sum==-sumValue))
+		{
+			if(diag2Sum==sumValue)
+				returnStruct.victoryFlag = 1;
+			else //diag1Sum==-sumValue
+				returnStruct.victoryFlag = -1;
+			// return -1 for both 
+			returnStruct.rowNumber = -1;
+			returnStruct.colNumber = -1;
+			returnStruct.diagNumber = 2;
+			return(returnStruct);
+		}
+	}
+
+	// no sumValue equivalence. victoryFlag=0
+	return(returnStruct);
+}
+
+int checkForVictory(int T3[][3])
+{	
+	// victoryFlag=0 => no victory
+	// victoryFlag=1 => user victory 
+	// victoryFlag=2 => my victory
+	checkSumReturn returnStruct = checkForSumValue(T3,3);
+	
+
+	return(returnStruct.victoryFlag);
+}
 
 void getUserInput(int T3[][3])
 {
@@ -55,46 +176,126 @@ void getUserInput(int T3[][3])
 	T3[rowNumber][colNumber] = 1;
 }
 
-int checkForVictory(int T3[][3])
+void calcMyInput(int T3[][3])
 {
-	int victoryFlag = 0;
-	// 8 possible victory scenarios
-	// row-wise
-	for(int i=0;i<3;i++)
-	{
-	 victoryFlag = victoryFlag||((T3[i][0]+T3[i][1]+T3[i][2])==3);
-	}
+	int rowNumber, colNumber;
+	
+	// calculate to check if there is impending victory
+	checkSumReturn returnStruct = checkForSumValue(T3,2);
+	if(returnStruct.victoryFlag) // impending victory
+	{	//block it or win it
+		//check if row victory
+		if((returnStruct.colNumber==-1)&&(returnStruct.diagNumber==-1))
+		{	// find empty element in row
+			for(int i=0; i<3;i++)
+			{	if(T3[returnStruct.rowNumber][i]==0)
+				{	T3[returnStruct.rowNumber][i]=-1;
+					return;
+				}
+			}
+		} // end row victory
 
-	// column-wise
-	for(int i=0;i<3;i++)
-	{
-	 victoryFlag = victoryFlag||((T3[0][i]+T3[1][i]+T3[2][i])==3);
-	}
 
-	// diagonal
-	 victoryFlag = victoryFlag||((T3[0][0]+T3[1][1]+T3[2][2])==3);
-	 victoryFlag = victoryFlag||((T3[0][2]+T3[1][1]+T3[2][0])==3);
+		//check if col victory
+		if((returnStruct.rowNumber==-1)&&(returnStruct.diagNumber==-1))
+		{	// find empty element in col
+			for(int i=0; i<3;i++)
+			{	if(T3[i][returnStruct.colNumber]==0)
+				{	T3[i][returnStruct.colNumber]=-1;
+					return;
+				}
+			}
+		} // end col victory
 
+		//check if diag victory
+		if((returnStruct.colNumber==-1)&&(returnStruct.rowNumber==-1))
+		{	
+			if(returnStruct.diagNumber==1)// forward diagonal
+			{	// find empty element in diagonal
+				for(int i=0; i<3;i++)
+				{	if(T3[i][i]==0)
+					{	T3[i][i]=-1;
+						return;
+					}
+				}
+			}
+			else // reverse diagonal
+			{	// find empty element in diagonal
+				for(int i=0; i<3;i++)
+				{	if(T3[i][2-i]==0)
+					{	T3[i][2-i]=-1;
+						return;
+					}
+				}
+			}			
+		} // end diag victory
 
-return victoryFlag;
+	} // if impending victory
+	else // no impending victory
+	{	
+		// occupy center element if empty
+		if(T3[1][1]==0)
+		{	T3[1][1]=-1;
+			return;
+		}
+		else
+		{
+			// find the first empty element
+			for(int i=0; i<3; i++)
+				for(int j=0; j<3; j++)
+				{	if(T3[i][j]==0)
+					{	T3[i][j]=-1;
+						return;
+					}
+				}// end for
+		}// end if/else inside no impending victory
+	}// end if/else impending victory check
 }
 
 
+
 int main()
-{
-	// TicTacToe matrix
-	int T3[3][3] = {0};
+{	
+	int playFlag=1;
 
-	// Display empty
-	dispT3(T3);
+	while(playFlag)
+	{
+		// TicTacToe matrix
+		int T3[3][3] = {0};
+		int emptyLocations=9;
 
-	int victoryFlag=0;
-
-	while(!victoryFlag)
-	{	// Ask for user input
-		getUserInput(T3);
+		// Display empty
 		dispT3(T3);
-	}
 
+		int victoryFlag=0;
+
+		while((!victoryFlag)&&(emptyLocations>0))
+		{	// Ask for user input
+			getUserInput(T3);		
+			dispT3(T3);
+			emptyLocations--; // one less empty location		
+			victoryFlag = checkForVictory(T3);
+
+			if((!victoryFlag)&&(emptyLocations>0))
+			{	cout << "My Turn :";
+				calcMyInput(T3);		
+				dispT3(T3);
+				emptyLocations--; // one less empty location
+			}
+		}
+
+		if(victoryFlag==1)
+			cout << "You Win!!" << endl;
+		else if(victoryFlag==-1)
+			cout << "I win!!" << endl;
+		else//no more empty spaces
+			cout << "tame draw" << endl << endl;
+
+		cout << "Do you want to play again? press 'y' for yes, any other key for no : ";
+		char userInput;
+		cin >> userInput;
+		if(userInput!='y')
+			playFlag=0;
+	}
 	return 0;
 }
